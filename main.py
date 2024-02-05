6c12fa8a0ee7812597f7ea335b9fb2fc65f0a4cc
esteban-brion <estebanbrion@gmail.com>
2024-02-04 19:32:12 -0500

    Question 3 et 4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 numero_symetrique = int(input("Entrez votre numéro du profil NACA 4 chiffres symétrique NACA00XX. Entrez uniquement les deux derniers chiffres : "))
 corde = float(input("Entrez la corde de votre profil (en mètres) : "))
@@ -21,6 +22,22 @@ y_coordinates = 5 * corde * (0.2969 * np.sqrt(x_coordinates) - 0.126 * x_coordin
 y_up = y_coordinates*corde
 y_down = (-1)*y_coordinates*corde
 # Affichage des coordonnées
+
+epaisseur_maximale = np.max(y_coordinates)
+position_epaisseur_maximale = x_coordinates[np.argmax(y_coordinates)]
+
+print("Épaisseur maximale :", epaisseur_maximale)
+print("Position de l'épaisseur maximale le long de la corde :", position_epaisseur_maximale)
+
 print("Coordonnées x:", x_coordinates)
 print("Coordonnées y:", y_coordinates)
 
+plt.plot(x_up, y_up, label='extrados',color ='r')
+plt.plot(x_down, y_down, label='intrados',color='b')
+plt.axhline(0, color='black', linewidth=0.8)  # Ligne horizontale pour l'axe x
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.title(f"Profil NACA00{numero_symetrique}")
+plt.xlabel('Position le long de la corde')
+plt.ylabel('Épaisseur')
+plt.legend()
+plt.show()

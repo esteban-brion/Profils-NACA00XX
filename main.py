5a050c7ef5b457ab1aae3202aebf436640795468
esteban-brion <estebanbrion@gmail.com>
2024-02-04 19:07:29 -0500

    Question 1

---
+++ main.py
@@ -0,0 +1,5 @@
+numero_symétriques = int(input("Entrez votre numéro du profil NACA 4 chiffres symétrique NACA00XX. Entrez uniquement les deux derniers chiffres :"))
+corde = float(input("entrez la corde de votre profil (en mètres) :"))
+nombre_points = int(input("Entrez le nombre de points que vous souhaitez afficher :"))
+distribution = input("Souhaitez vous une distribution linéaire ou non linéaire ? 'o' pour oui 'n' pour non :")
+
